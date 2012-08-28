d46024a0837cecb611c1468ca7364f01aac9dd94
Peter Anderson <diniremix@gmail.com>
2012-08-28 17:44:21 -0500

    agregado la clase para crear el archivo con la plantilla de codigo

--- projectwizard.py
+++ projectwizard.py
@@ -1,5 +1,14 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, os
 
-class ExampleCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		self.view.insert(edit, 0, "Hola mundo!")
+class PythonCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		self.path=os.path.join(sublime.packages_path(), 'User')
+		v = self.window.new_file()
+		v.settings().set('default_dir',self.path)
+		v.settings().set('default_extension', 'source.python')
+		v.set_syntax_file('Packages/Python/Python.tmLanguage')
+		v.set_name('untitled.py')		
+		template="""import os
+print "hola python y sublime text 2"
+"""
+		v.run_command("insert_snippet", {"contents": template})
